8432351b544dbf6b31be3c431059be6d143a815e
armanhamza <armanhamza2323@gmail.com>
2026-09-17 20:38:09 +0500

    Compile Together.

---
+++ Area.java
@@ -0,0 +1,8 @@
+public class Area {
+    public static void main(String args[]){
+        Circle c1 = new Circle();
+        c1.radius=10;
+        System.out.println(c1);
+        System.out.println(c1.radius);
+    }
+}
